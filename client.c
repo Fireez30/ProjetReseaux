d1b172facf68243d95d069dbd256e0b59e1ceec1
Fireez30 <32301553+Fireez30@users.noreply.github.com>
2017-12-07 12:12:47 +0100

    Add files via upload

--- client.c
+++ client.c
@@ -16,22 +16,22 @@ if (argc!=4){
 	return -1;
 }
 
-char* nomfichier=argv[3];
+char* nomfichier=argv[3];//le nom du fichier en parametre
 nomfichier[strlen(nomfichier)]='\0';
 
 printf("Nom du fichier à envoyer : %s\n",nomfichier);
 
-int dSock=socket(PF_INET,SOCK_STREAM,0);
+int dSock=socket(PF_INET,SOCK_STREAM,0);// je creer la socket de mon coté
 if (dSock<0) {
 	printf("Erreur lors de la creation de la socket ! ");
 	return -1;
 }
 
-struct sockaddr_in adR;
+struct sockaddr_in adR;// création de ladresse
 adR.sin_family=AF_INET;
-adR.sin_port=htons(atoi(argv[2]));
+adR.sin_port=htons(atoi(argv[2])); // le port du serveur est a passé en parametre
 
-int res =inet_pton(AF_INET,argv[1],&(adR.sin_addr));
+int res =inet_pton(AF_INET,argv[1],&(adR.sin_addr));//On va stocker l'adresse sous forme réseau 
 if (res!=1) {
 	printf("Erreur lors du stockage de l'adresse !");
 	return -1;
@@ -42,7 +42,7 @@ char adressefic[4096];
 adressefic[4095]='\0';
 
 strcat(adressefic,"envoi/");
-strcat(adressefic,nomfichier);
+strcat(adressefic,nomfichier);// adresse du fichier a envoyer
 
 fp =fopen(adressefic,"r");
 
@@ -52,50 +52,65 @@ if (stat(adressefic,&sb)==-1) {
     exit(EXIT_FAILURE);
 }
 
-long taille=(long) sb.st_size;
+long taille=(long) sb.st_size;//stockage de la taille du fichier pour l'envoyer
 printf("Taille du fichiers en octets: %ld\n",taille);
 
-char buffer[taille];
-fread(buffer,taille,1,fp);
-fclose(fp);
-printf("Lu : %s\n", buffer);
+
 
 socklen_t lgAd=sizeof(adR);
-int conn=connect(dSock,(struct sockaddr *) &adR,lgAd);
+int conn=connect(dSock,(struct sockaddr *) &adR,lgAd);// je me connecte a la socket du server 
 if (conn == -1){
 	printf("Erreur lors de la connection !");
 	return -1;
 }
 
-int res1=send(dSock,&taille,sizeof(int),0);
+int res1=send(dSock,&taille,sizeof(int),0);// j'envoie la taille totale du fichier au serveur
 if (res1 == -1){
 	printf("Erreur lors de l'envoi !");
 	return -1;
 }
 printf("Taille de la donnee envoyee (taille fichier) : %d\n",res1);
 
-int taille1=(int)(strlen(nomfichier));//taille du nom du fichier a envoyer
+int taille1=(int)(strlen(nomfichier));//taille du nom du fichier a envoyer 
 
-int res4=send(dSock,&taille1,sizeof(int),0);
+int res4=send(dSock,&taille1,sizeof(int),0);//j'envoie la taille du nom
 if (res4 == -1){
 	printf("Erreur lors de l'envoi de la taille du nom du fic ! ");
 	return -1;
 }
 
-int res2=send(dSock,buffer,taille,0);//Envoi du contenu
-if (res2==-1){
-	printf("Erreur lors de l'envoi ! ");
-	return -1;
-}
-printf("Taille envoyée : %d\n",res2);
-
-int res3=send(dSock,nomfichier,strlen(nomfichier),0);
+int res3=send(dSock,nomfichier,strlen(nomfichier),0);//j'envoie le nom du fichier au serveur
 if (res3 == -1){
 	printf("Erreur lors de l'envoi du nom !");
 	return -1;
 }
 
-close(dSock);
+char buffer[100];
+int acclus = 0;
+
+while (acclus < taille){// tant qu'on peut lire dans le fichier
+
+	int nblus = fread(&buffer,1,100,pf);// je lis jusqua 100 caractere dans le fichier
+	if (nblus == -1){
+		printf("Erreur lors de la lecture dans le fichier !"),
+		return -1;
+	}
+	printf("Lu : %s\n", buffer);
+
+	int res2=send(dSock,buffer,sizeof(buffer),0);//j'envoi ce que j'ai lu au serveur
+	if (res2 == -1){
+		printf("Erreur lors de l'envoi partie !");
+		return -1;
+	}
+
+	acclus += res2;//pour arreter le while
+	res2 = 0;
+}
+
+printf("Fichier envoyé ! ");
+
+fclose(fp);//je ferme mon fichier
+close(dSock);//je ferme la socket car communication terminée
 
 	return 0;
 }
